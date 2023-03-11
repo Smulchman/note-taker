883d300fa124dbeb8df621092cb01c8d8e0860f5
smulch <mulcahy.samuel@gmail.com>
2023-03-10 22:58:13 -0500

    added disclaimer to all pages, instead of just the one, acknowledging influence of the solution that was pushed onto the repo

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,3 +1,5 @@
+// please note: the solution to the challenge was put onto our repo early and I started this repository by copying that code. I removed all the code they included but I have kept the file names. I am also working along side the solution while trying my best to only use the things that I understand.
+
 const router = require('express').Router();
 
 router.get('/api/notes', (req, res) => {
